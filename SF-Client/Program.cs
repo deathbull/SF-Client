8831a93bdd4e19675ede5f50eccedc642f220d9d
deathbull <48456995+deathbull@users.noreply.github.com>
2020-07-23 00:00:51 +0300

    v1.1.1
    Browser's javascript wasn't working ( Fixed )

--- SF-Client/Program.cs
+++ SF-Client/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace SF_Client
 {
@@ -10,6 +11,13 @@ namespace SF_Client
         [STAThread]
         static void Main()
         {
+            //Confirmation for Webbrowser to use javascript
+            RegistryKey reg = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION\", true);
+            if (reg == null)
+                reg = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Internet Explorer\Main\FeatureControl\FEATURE_BROWSER_EMULATION\");
+            if (reg.GetValue("SF-Client.exe") == null)
+                reg.SetValue("SF-Client.exe", 0x00002af9);
+
             CosturaUtility.Initialize();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
